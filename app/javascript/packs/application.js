089c4671a981ccc13dee326d86f41948aeb4f059
Vincentius Nicky Wijaya <vincentius.nicky@bukalapak.com>
2025-02-25 10:30:17 +0700

    implement simple type 1 with no error

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,22 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import JQuery from 'jquery';
+window.$ = window.JQuery = JQuery;
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import SmoothScroll from 'smooth-scroll';  // Import the smooth-scroll library
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+// scrolling effect
+document.addEventListener("DOMContentLoaded", function() {
+  // Initialize SmoothScroll for links with href that contain #
+  var scroll = new SmoothScroll('a[href*="#"]');
+});
