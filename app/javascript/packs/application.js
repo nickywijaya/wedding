12b2e4a6f1518d71b1b9b3ea1aa423a12b31d91a
Vincentius Nicky Wijaya <vincentius.nicky@bukalapak.com>
2025-02-25 09:10:03 +0700

    first init untill error jQuery not defined

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,28 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import $ from 'jquery';
+global.$ = global.jQuery = $;
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+
+import './google_map.js';
+import './jquery.countTo.js';
+import './jquery.easing.1.3.js';
+import './jquery.magnific-popup.min.js';
+import './jquery.min.js';
+import './jquery.stellar.min.js';
+import './jquery.waypoints.min.js';
+import './magnific-popup-options.js';
+import './main.js';
+import './modernizr-2.6.2.min.js';
+import './owl.carousel.min.js';
+import './respond.min.js';
+import './simplyCountdown.js';
+import './bootstrap.min.js';
 
 Rails.start()
 Turbolinks.start()
