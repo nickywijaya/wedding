427a9b9d59bf06a4156ce9294279bf863e2f3761
Vincentius Nicky Wijaya <vincentius.nicky@bukalapak.com>
2025-02-25 09:47:59 +0700

    update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,28 +3,36 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import $ from 'jquery';
-global.$ = global.jQuery = $;
+import JQuery from 'jquery';
+import JQueryUJS from 'jquery-ujs';
+window.$ = window.JQuery = JQuery;
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+
+JQueryUJS.start()
+
+import './main.js';
 import './google_map.js';
 import './jquery.countTo.js';
 import './jquery.easing.1.3.js';
-import './jquery.magnific-popup.min.js';
 import './jquery.min.js';
-import './jquery.stellar.min.js';
+
+
+
+import './simplyCountdown.js';
+import './bootstrap.min.js';
+import './jquery.magnific-popup.min.js';
 import './jquery.waypoints.min.js';
-import './magnific-popup-options.js';
-import './main.js';
+import './jquery.stellar.min.js';
 import './modernizr-2.6.2.min.js';
 import './owl.carousel.min.js';
 import './respond.min.js';
-import './simplyCountdown.js';
-import './bootstrap.min.js';
+import './magnific-popup-options.js';
+
 
 Rails.start()
 Turbolinks.start()
