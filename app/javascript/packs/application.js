4117cbd5bb1bf54a4ecc7f72378d9cb5a4a59afb
Vincentius Nicky Wijaya <vincentius.nicky@bukalapak.com>
2025-02-26 14:34:05 +0700

    first init

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,21 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import JQuery from 'jquery';
+window.$ = window.JQuery = JQuery;
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+
+import './bootstrap.min'
+import './click-scroll'
+import './custom'
+import './jquery.min'
+import './magnific-popup-options'
+import './magnific-popup.min'
+import './sticky'
 
 Rails.start()
 Turbolinks.start()
