6d18def1dcf482c7aa1346348a9147029880254b
Vincentius Nicky Wijaya <vincentius.nicky@bukalapak.com>
2025-02-28 13:21:03 +0700

    make local js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,11 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import JQuery from 'jquery';
+window.$ = window.JQuery = JQuery;
+
+import 'owl.carousel';
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -11,3 +16,13 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import 'css/site.scss'
+import './jquery.min'
+import './bootstrap.min'
+import './easing.min'
+import './isotope'
+import './lightbox.min'
+import './main'
+import './owl.carousel.min'
+import './waypoints.min'
